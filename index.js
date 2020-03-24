8d659716502b971abdd7c5ec9ba005009c964399
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-24 21:44:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-function titleCased(){
+function titleCased(tutorials){
   const titledTutorials = tutorials.map(tutorial => {
     let splitString = tutorial.split(' ')
     let newSentence = splitString.map(word => word.charAt(0).toUpperCase() + word.slice(1))
