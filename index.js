ebc13d25b60b1b1045594d5fa7a96af7be111846
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-24 21:45:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const titleCased = () => {
     let splitString = tutorial.split(' ')
     let newSentence = splitString.map(word => word.charAt(0).toUpperCase() + word.slice(1))
     let titledTutorial = newSentence.join(' ')
+    return titleCasedTutorial
   })
   return titledTutorials
 }
